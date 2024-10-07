905bfb64e95954809dae7c1150196eaab57b081f
Abdulmalik105 <abdulmalikaai15@gmail.com>
2024-10-07 21:43:02 +0100

    Add files via upload

---
+++ Ass1.js
@@ -0,0 +1,21 @@
+function calculate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+        case '×':
+            return num1 * num2;
+        case '/':
+            if (num2 !== 0) {
+                return num1 / num2;
+            } else {
+                return 'Error: Division by zero';
+            }
+        case '%':
+            return num1 % num2;
+        default:
+            return 'Error: Invalid operator';
+    }
+}
